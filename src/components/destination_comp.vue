59930e016768fe5d9638181603c7b131a20c6dd8
Karl Erik Mander <manderka@ut.ee>
2021-12-13 16:05:49 +0200

    final version

--- src/components/destination_comp.vue
+++ src/components/destination_comp.vue
@@ -1,55 +1,64 @@
 <template>
-<div id= "destination-list-one">
-
-</div>
+  <div id="destination-list-one">
+    <div v-for="(singleDest, index) in destinationList" :key="index">
+      <p>From {{singleDest["from"]}} to
+        {{singleDest["to"]}} at {{singleDest["time"]}}
+      with distance {{singleDest["distance"]}}km with price of {{singleDest["price"]}}€
+      </p>
+      <p>Tickets left: {{singleDest["tickets"]}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "destination_comp",
-data: function() {
-return {
+  name: "destination_comp",
+  props: {
+    destinationList: {
+      type: Array,
+      required: true
+    }
+  },
+  data: function() {
+    return {};
+  },
 
+  computed: {},
 
-}},
+  methods: {}
 
-computed: {
-
-}, 
-
- methods: {
-
-}  
-
-}
+};
 </script>
 
 
-
 <style scoped>
-.item{
-background: rgb(47, 255, 141);
-margin-bottom: 5px;
-padding: 3px 5px;
+.item {
+  background: rgb(47, 255, 141);
+  margin-bottom: 5px;
+  padding: 3px 5px;
 }
-#comp-list-one{
-background: #d9b1ff;
-box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
-margin-bottom: 30px;
-padding: 10px 20px;
+
+#comp-list-one {
+  background: #d9b1ff;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
 }
-#comp-list-one ul{
-padding: 0;
+
+#comp-list-one ul {
+  padding: 0;
 }
-#comp-list-one li{
-display: inline-block;
-margin-right: 10px;
-margin-top: 10px;
-padding: 20px;
-background: rgba(255,255,255,0.7);
+
+#comp-list-one li {
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
 }
-.price{
-font-weight: bold;
-color: #E8800C;
+
+.price {
+  font-weight: bold;
+  color: #E8800C;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import about from "../views/About.vue"
+import destinations from "../views/Destinations.vue"
 Vue.use(VueRouter);
 
 const routes = [{
@@ -15,8 +16,12 @@ const routes = [{
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ "../views/About.vue"),
+        component: about,
+    },
+    {
+      path: "/destinations",
+      name: "Destinations",
+      component: destinations,
     },
 ];
 
